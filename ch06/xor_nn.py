9b299410063448dcf161bac47b1cbb06f2b3efbd
mskims <mskim@quicket.co.kr>
2017-03-04 00:53:07 +0900

    Use neural network to solve XOR EXAM

--- ch06/xor_nn.py
+++ ch06/xor_nn.py
@@ -1,25 +1,21 @@
 import tensorflow as tf
 import numpy as np
 
-
-# XOR EXAM
-"""
-    2017, Mar, 3 Fri
-    Accuracy is can not be close at 1.0.
-    we can now know limit of Logistic regression.
-"""
-
 xy_data = np.loadtxt('xor.txt', unpack=True)
-x_data = xy_data[0:-1]
-y_data = xy_data[-1]
+x_data = np.transpose(xy_data[0:-1])
+y_data = np.reshape(xy_data[-1], (4, 1))
 
 X = tf.placeholder(tf.float32)
 Y = tf.placeholder(tf.float32)
-W = tf.Variable(tf.random_uniform([1, len(x_data)], -1.0, 1.0))
 
-h = tf.matmul(W, X)
-# Sigmoid Function
-hypothesis = tf.div(1., 1 + tf.exp(-h))
+b1 = tf.zeros([2], name='Bias1')
+b2 = tf.zeros([1], name='Bias2')
+
+W1 = tf.Variable(tf.random_uniform([2, 2], -1.0, 1.0))
+W2 = tf.Variable(tf.random_uniform([2, 1], -1.0, 1.0))
+
+L2 = tf.sigmoid( tf.matmul(X, W1) + b1)
+hypothesis = tf.sigmoid( tf.matmul(L2, W2) + b2)
 
 cost = -tf.reduce_mean(Y * tf.log(hypothesis) + (1 - Y) * tf.log(1 - hypothesis))
 
@@ -34,10 +30,10 @@ fd = {
     X: x_data,
     Y: y_data,
 }
-for step in xrange(2001):
+for step in xrange(100000):
     sess.run(train, feed_dict=fd)
-    if step % 20 == 0:
-        print(step, sess.run(W), sess.run(cost, feed_dict=fd))
+    if step % 200 == 0:
+        print(step, sess.run([W1, W2]), sess.run(cost, feed_dict=fd))
 
 # Test model
 correct_prediction = tf.equal(tf.floor(hypothesis + 0.5), Y)
